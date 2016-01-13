e73b5213f588d85540e7edacead1ea7f1a624213
Alex Lauderdale <alexlauderdale@MBP-Retina-833.local>
2016-01-12 20:54:11 -0700

    more

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -51,5 +51,23 @@ $(document).ready(function(){
     
 
 
+    //smooth scroll
+
+    $(function() {
+          $('a[href*=#ss]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+        });
+
+
 
 });
